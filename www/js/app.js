a01eaad189af8b4c5386626b1c296b0aafe75f3e
amad-person <e0072565@u.nus.edu>
2017-06-16 19:54:13 +0530

    added sidemenu and cleaned up code

--- www/js/app.js
+++ www/js/app.js
@@ -1,39 +1,65 @@
-angular.module('NUSTalk', ['ionic', 'NUSTalk.controllers'])
+angular.module('NUSTalk', ['ionic', 'NUSTalk.controllers', 'NUSTalk.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  $stateProvider
-
-  .state('Home', {
-    url: '/home',
-    templateUrl: 'templates/Homepage.html',
-    controller: 'HomeController'
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
   })
 
-  .state('Module', {
-    url: '/module/:name',
-    templateUrl: 'templates/Module.html',
-    controller: 'ModuleController'
-  });
+  .config(function($stateProvider, $urlRouterProvider) {
 
-  // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/home');
-});
+    $stateProvider
+      .state('NUSTalk', {
+        url: '/NUSTalk',
+        abstract: true,
+        templateUrl: 'templates/sidemenu.html',
+        controller: 'AppController'
+      })
+
+      .state('NUSTalk.home', {
+        url: '/home',
+        views: {
+          'NUSTalkSideMenu': {
+            templateUrl: 'templates/homepage.html',
+            controller: 'HomeController'
+          }
+        }
+      })
+
+      .state('NUSTalk.module', {
+        url: '/module/:moduleName',
+        views: {
+          'NUSTalkSideMenu': {
+            templateUrl: 'templates/module.html',
+            controller: 'ModuleController',
+            params: {moduleName: null}
+          }
+        }
+      })
+
+      .state('NUSTalk.info', {
+        url: '/module/:moduleName/info',
+        views: {
+          'NUSTalkSideMenu': {
+            templateUrl: 'templates/moduleInfo.html',
+            controller: 'InfoController',
+            params: {moduleName: null}
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/NUSTalk/home');
+  });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,16 +1,39 @@
 angular.module('NUSTalk.controllers', [])
+  .controller('AppController', function($scope) {
 
-.controller('HomeController', function($scope) {
-  $scope.modules = [
-    {id:1, name:'CS1010'},
-    {id:2, name:'CS1020'},
-    {id:3, name:'CS1030'},
-    {id:4, name:'CS1040'},
-    {id:5, name:'CS1050'}];
-})
+  })
 
-.controller('ModuleController', function($scope, $stateParams) {
-  $scope.currentModule = {
-    name: $stateParams.name
-  };
-});
+  .controller('LoginController', function($scope) {
+
+  })
+
+  .controller('HomeController', function($scope, $ionicModal) {
+    // TODO: get the users to select what modules they want to add
+    $scope.modules = [
+      {id:1, name:'CS1010'},
+      {id:2, name:'CS1020'},
+      {id:3, name:'CS1030'},
+      {id:4, name:'CS1040'},
+      {id:5, name:'CS1050'}];
+  })
+
+  .controller('ModuleController', function($scope, $state, $stateParams) {
+    $scope.currentModule = {
+      name: $stateParams.moduleName
+    };
+
+    $scope.goToInfo = function() {
+      $state.go('NUSTalk.info', { moduleName: $stateParams.moduleName });
+    }
+  })
+
+  .controller('InfoController', function($scope, $stateParams) {
+    $scope.currentModule = {
+      name: $stateParams.moduleName
+    }
+
+  })
+
+  .controller('SettingsController', function($scope) {
+
+  });
