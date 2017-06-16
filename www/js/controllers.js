7a87380a10cbec3d914e3446f16b85acc73f12a1
amad-person <e0072565@u.nus.edu>
2017-06-16 22:30:02 +0530

    added login/logout modals

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,58 @@
 angular.module('NUSTalk.controllers', [])
-  .controller('AppController', function($scope) {
+  .controller('AppController', function($scope, $ionicModal) {
+    $scope.hasLoggedIn = false;
 
+    // LOGIN MODAL
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.loginModal = modal;
+    });
+
+    // LOGOUT MODAL
+    $ionicModal.fromTemplateUrl('templates/logout.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.logoutModal = modal;
+    });
+
+    // close modal
+    var closeModal = function(hasLoggedIn) {
+      if(hasLoggedIn) {
+        $scope.logoutModal.hide();
+        $scope.logoutModal.remove();
+      } else {
+        $scope.loginModal.hide();
+        $scope.loginModal.remove();
+      }
+    };
+
+    // show login modal
+    var showModal = function(hasLoggedIn) {
+      if(hasLoggedIn) {
+        $scope.logoutModal.show();
+      } else {
+        $scope.loginModal.show();
+      }
+    };
+
+    $scope.loginOrLogout = function(hasLoggedIn) {
+      if(hasLoggedIn) {
+        // show logout
+        showModal(hasLoggedIn);
+        //TODO: update flag only if user logs out
+        $scope.hasLoggedIn = false;
+      } else {
+        // show login
+        showModal(hasLoggedIn);
+        //TODO: update flag only if user logs in
+        $scope.hasLoggedIn = true;
+      }
+    }
   })
 
-  .controller('LoginController', function($scope) {
-
-  })
-
-  .controller('HomeController', function($scope, $ionicModal) {
-    // TODO: get the users to select what modules they want to add
+  .controller('HomeController', function($scope) {
+    // TODO: get the users to select what modules they want to add from the NUSMods API's JSON objects
     $scope.modules = [
       {id:1, name:'CS1010'},
       {id:2, name:'CS1020'},
